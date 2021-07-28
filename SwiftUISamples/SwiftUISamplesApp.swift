5fc5fb5aa0bb0f1f0b0e3bcce76d6c984502b388
Jad Abi Abdallah <JAbi-Abdallah.prestataire@oui.sncf>
2021-07-28 09:41:47 +0200

    Initial Commit

---
+++ SwiftUISamples/SwiftUISamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISamplesApp.swift
+//  SwiftUISamples
+//
+//  Created by Abi-Abdallah Jad on 28/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
